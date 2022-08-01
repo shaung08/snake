de2c48fd317a67bf5a50c2346a5c777cbc17d0e7
shaung08 <a2369875@gmail.com>
2022-08-01 15:12:17 +0800

    add thread architecture

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
+#include <thread>
 
 #include "canvas.h"
 #include "canvas.cpp"
@@ -12,9 +13,58 @@
 #define width 50
 #define height 25
 
+void snake_thread(*canvdraw.background) {
+    // set moving speed
+    speed = 0.01 // 0.01s move a pixel
+    while (true) {
+        // move snake regular
+
+        // put canvas background into blocking queue
+
+        // set delay time
+    }
+}
+
+void keyboard_thread() {
+    keyboard userkey;
+    char key;
+    while (true) {
+        // get key
+        key = userkey.get_keyboard();
+        // get direction
+        direction = userkey.get_direction(&key);
+        // save direction to blocking queue
+        if(snakebody.key_move(*canvdraw.background, direction)) {
+            // put canvas background into blocking queue
+
+            continue;
+        }
+        else {
+            printf("----------hit the snake--------\nend\n");
+            break;
+        }
+    }
+
+}
+
+void canvas_thread() {
+  while(true) {
+    // get canvas background blocking queue
+
+    // show canvas background
+    canvdraw.show_canvas();
+  }
+}
 
 
 int main () {
+    canvas canvdraw;
+  
+    std::thread snake_thread(*canvdraw.background);
+    std::thread keyboard_thread(*canvdraw.background);
+    std::thread canvas_thread();
+
+    /*
     canvas canvdraw;
     snake snakebody(*canvdraw.background, std::floor(height/2), std::floor(width/2));
     keyboard userkey;
@@ -23,6 +73,7 @@ int main () {
     char key;
     int direction;
     bool status;
+
     while (true)
     {
         key = userkey.get_keyboard();
@@ -36,6 +87,7 @@ int main () {
             break;
         }
     }
+    */
     
 
     return 0;
