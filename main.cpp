d3808fce3903bfa9bc7e9a711e80d4af0e8a5667
shaung08 <a2369875@gmail.com>
2022-09-10 15:39:10 +0800

    add mutex

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <unistd.h>
 #include <mutex>
+#include <time.h>
 
 #include "canvas.h"
 #include "canvas.cpp"
@@ -20,6 +21,8 @@ snake snakebody(*canvdraw.background, std::floor(height/2), std::floor(width/2))
 std::mutex t_mutex;
 int direction;
 bool alive = true;
+clock_t start,end;
+float speed = 1; // 0.01s move a pixel
 
 void show_canvas() {
     if(snakebody.move_snake(*canvdraw.background, direction)) {
@@ -35,14 +38,15 @@ void show_canvas() {
 
 void snake_thread(char* background) {
     // set moving speed
-    float speed = 0.01; // 0.01s move a pixel
     while (alive) {
-        // move snake regular
-        t_mutex.lock();
-        direction = snakebody.snake_coor.dir_list[snakebody.snake_coor.dir_list.size()-1];
-        show_canvas();
-        t_mutex.unlock();
-        sleep(1);
+        if (double(clock() - start )/CLOCKS_PER_SEC > 1.0) {
+            t_mutex.lock();
+            // move snake regular
+            direction = snakebody.snake_coor.dir_list[snakebody.snake_coor.dir_list.size()-1];
+            show_canvas();
+            t_mutex.unlock();
+            sleep(speed);
+        }
     }
 }
 
@@ -58,11 +62,13 @@ void keyboard_thread(char* background) {
         std::cout << "key_move_snake\n";
         direction = userkey.get_direction(&key);
         show_canvas();
+        start = clock(); 
         t_mutex.unlock();
     }
 }
 
 int main () {
+    start = clock(); 
     std::thread s_thread(snake_thread, *canvdraw.background);
     std::thread k_thread(keyboard_thread, *canvdraw.background);
     s_thread.join();
